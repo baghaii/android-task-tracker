796d8c8193a31a002440880f7ea72eaac2660fcc
Baghaii <baghaii@gmail.com>
2016-06-23 11:54:55 -0600

    Google Calendar API
    Added files to make Google Calendar changes.  It is not working, but it is not spewing errors either.

--- app/src/main/java/com/sepidehmiller/android_task_tracker/CalendarRequestTask.java
+++ app/src/main/java/com/sepidehmiller/android_task_tracker/CalendarRequestTask.java
@@ -1,8 +1,62 @@
 package com.sepidehmiller.android_task_tracker;
 
+import android.os.AsyncTask;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventDateTime;
+import java.io.IOException;
+
 /**
  * Created by baghaii on 6/13/16.
  */
-public class CalendarRequestTask {
 
+
+public class CalendarRequestTask extends AsyncTask<Task, Void, Boolean> {
+    private com.google.api.services.calendar.Calendar mService = null;
+    private Exception mLastError = null;
+
+    public CalendarRequestTask(GoogleAccountCredential credential) {
+        HttpTransport transport = AndroidHttp.newCompatibleTransport();
+        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+        mService = new com.google.api.services.calendar.Calendar.Builder(
+                transport, jsonFactory, credential)
+                .setApplicationName("TaskTracker")
+                .build();
+    }
+
+    /**
+     * Background task to call Google Calendar API.
+     * @tasks has the date, title, summary of the event.
+     */
+    @Override
+    protected Boolean doInBackground(Task... tasks) {
+        try {
+            setEventInApi(tasks);
+            return true;
+        } catch (Exception e) {
+            mLastError = e;
+            cancel(true);
+            return false;
+        }
+    }
+
+    private void setEventInApi(Task... tasks) throws IOException {
+        // Insert an event into the Google Calendar
+
+        for (Task task: tasks) {
+            Event event = new Event()
+                    .setSummary(task.getTitle())
+                    .setDescription(task.getDescription());
+            DateTime startTime = new DateTime(task.getDueDate());
+            EventDateTime start = new EventDateTime()
+                    .setDateTime(startTime);
+            event.setStart(start);
+            mService.events().insert("primary", event).execute();
+        }
+    }
 }
--- app/src/main/java/com/sepidehmiller/android_task_tracker/TaskFragment.java
+++ app/src/main/java/com/sepidehmiller/android_task_tracker/TaskFragment.java
@@ -1,8 +1,12 @@
 package com.sepidehmiller.android_task_tracker;
 
+import android.Manifest;
+import android.accounts.AccountManager;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -32,6 +36,7 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
 
 /**
@@ -42,10 +47,17 @@ public class TaskFragment extends Fragment implements EasyPermissions.Permission
     private static final String ARG_TASK_ID = "task_id";
     private static final String DIALOG_DATE = "DialogDate";
     private static final String DIALOG_TIME = "DialogTime";
-    private static final String[] SCOPES = { CalendarScopes.CALENDAR_READONLY };
+    private static final String[] SCOPES = { CalendarScopes.CALENDAR };
 
     private static final int REQUEST_DATE = 0;
     private static final int REQUEST_TIME = 1;
+    private static final int REQUEST_ACCOUNT_PICKER = 1000;
+    private static final int REQUEST_AUTHORIZATION = 1001;
+    private static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    private static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
+
+    private static final String PREF_ACCOUNT_NAME = "accountName";
+
 
     GoogleAccountCredential mCredential;
 
@@ -98,7 +110,7 @@ public class TaskFragment extends Fragment implements EasyPermissions.Permission
                 getActivity().finish();
                 return true;
             case R.id.menu_item_to_calendar:
-                getResultFromApi();
+                getResultsFromApi();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -106,17 +118,56 @@ public class TaskFragment extends Fragment implements EasyPermissions.Permission
     }
 
     /** Attempt to call the Google Calendar API **/
-    private void getResultFromApi() {
+    private void getResultsFromApi() {
         if (!isGooglePlayServicesAvailable() ) {
+            acquireGooglePlayServices();
             Toast.makeText(getActivity(), "Services Unavailable", Toast.LENGTH_SHORT).show();
         } else if (mCredential.getSelectedAccount() == null) {
+            chooseAccount();
             Toast.makeText(getActivity(), "Pick an account", Toast.LENGTH_SHORT).show();
         } else if (! isDeviceOnline()) {
             Toast.makeText(getActivity(), "Connect to the internet", Toast.LENGTH_SHORT).show();
         } else {
-
+            new CalendarRequestTask(mCredential).execute(mTask);
+            Toast.makeText(getActivity(), "Finally! do something useful.", Toast.LENGTH_SHORT).show();
         }
     }
+
+    /**
+     * Attempts to set the account used with the API credentials. If an account
+     * name was previously saved it will use that one; otherwise an account
+     * picker dialog will be shown to the user. Note that the setting the
+     * account to use with the credentials object requires the app to have the
+     * GET_ACCOUNTS permission, which is requested here if it is not already
+     * present. The AfterPermissionGranted annotation indicates that this
+     * function will be rerun automatically whenever the GET_ACCOUNTS permission
+     * is granted.
+     */
+    @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
+    private void chooseAccount() {
+        if (EasyPermissions.hasPermissions(
+                getActivity(), Manifest.permission.GET_ACCOUNTS)) {
+            String accountName = getActivity().getPreferences(Context.MODE_PRIVATE)
+                    .getString(PREF_ACCOUNT_NAME, null);
+            if (accountName != null) {
+                mCredential.setSelectedAccountName(accountName);
+                getResultsFromApi();
+            } else {
+                // Start a dialog from which the user can choose an account
+                startActivityForResult(
+                        mCredential.newChooseAccountIntent(),
+                        REQUEST_ACCOUNT_PICKER);
+            }
+        } else {
+            // Request the GET_ACCOUNTS permission via a user dialog
+            EasyPermissions.requestPermissions(
+                    this,
+                    "This app needs to access your Google account (via Contacts).",
+                    REQUEST_PERMISSION_GET_ACCOUNTS,
+                    Manifest.permission.GET_ACCOUNTS);
+        }
+    }
+
 
     /** Check whether the device curently has a connection. */
 
@@ -139,6 +190,37 @@ public class TaskFragment extends Fragment implements EasyPermissions.Permission
                 apiAvailability.isGooglePlayServicesAvailable(getContext());
         return connectionStatusCode == ConnectionResult.SUCCESS;
     }
+
+    /**
+     * Attempt to resolve a missing, out-of-date, invalid or disabled Google
+     * Play Services installation via a user dialog, if possible.
+     */
+    private void acquireGooglePlayServices() {
+        GoogleApiAvailability apiAvailability =
+                GoogleApiAvailability.getInstance();
+        final int connectionStatusCode =
+                apiAvailability.isGooglePlayServicesAvailable(getActivity());
+        if (apiAvailability.isUserResolvableError(connectionStatusCode)) {
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
+        }
+    }
+
+    /**
+     * Display an error dialog showing that Google Play Services is missing
+     * or out of date.
+     * @param connectionStatusCode code describing the presence (or lack of)
+     *     Google Play Services on this device.
+     */
+    void showGooglePlayServicesAvailabilityErrorDialog(
+            final int connectionStatusCode) {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        Dialog dialog = apiAvailability.getErrorDialog(
+                getActivity(),
+                connectionStatusCode,
+                REQUEST_GOOGLE_PLAY_SERVICES);
+        dialog.show();
+    }
+
 
     @Nullable
     @Override
@@ -217,19 +299,43 @@ public class TaskFragment extends Fragment implements EasyPermissions.Permission
             return;
         }
 
-        if (requestCode == REQUEST_DATE) {
-            Date date = (Date) data
-                    .getSerializableExtra(DatePickerFragment.EXTRA_DATE);
-            mTask.setDueDate(date);
-            updateDate();
+        Date date;
+
+        switch(requestCode) {
+            case REQUEST_DATE:
+                date = (Date) data
+                        .getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+                mTask.setDueDate(date);
+                updateDate();
+                break;
+            case REQUEST_TIME:
+                date = (Date) data
+                        .getSerializableExtra(TimePickerFragment.EXTRA_TIME);
+                mTask.setDueDate(date);
+                updateTime();
+                break;
+            case REQUEST_AUTHORIZATION:
+            case REQUEST_GOOGLE_PLAY_SERVICES:
+                getResultsFromApi();
+                break;
+            case REQUEST_ACCOUNT_PICKER:
+                if (data !=null && data.getExtras() != null) {
+                    String accountName =
+                            data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        SharedPreferences settings =
+                                getActivity().getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.putString(PREF_ACCOUNT_NAME, accountName);
+                        editor.apply();
+                        mCredential.setSelectedAccountName(accountName);
+                        getResultsFromApi();
+                    }
+                }
+                break;
         }
 
-        if (requestCode == REQUEST_TIME) {
-            Date date = (Date) data
-                    .getSerializableExtra(TimePickerFragment.EXTRA_TIME);
-            mTask.setDueDate(date);
-            updateTime();
-        }
+
     }
 
     private void updateDate() {
